9ae76306cca025cd295082a6c9c3c9daf3f826b4
Sandeep Batta <battasndp@gmail.com>
2023-06-09 17:43:23 +0530

    Daily Challenge 9th June 2023

---
+++ Daily Challenge/June 2023/Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,13 @@
+# # Challenge Context
+# You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+# Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+# Challenge Link --> https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: list[str], target: str) -> str:
+        for letter in letters:
+            if letter > target:
+                return letter
+        return letters[0]
