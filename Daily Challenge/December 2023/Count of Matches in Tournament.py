5883fea7ef428705d02b42ef9f8d6f86c5fba734
SandeepBatta <battasndp@gmail.com>
2023-12-05 17:47:44 -0500

    Daily Challenge 5th Dec 2023

---
+++ Daily Challenge/December 2023/Count of Matches in Tournament.py
@@ -0,0 +1,15 @@
+# # Challenge Context
+# You are given an integer n
+# Return the number of matches played in the tournament until a winner is decided.
+# Challenge Link --> https://leetcode.com/problems/count-of-matches-in-tournament/?envType=daily-question&envId=2023-12-05
+
+
+class Solution:
+    def numberOfMatches(self, n: int, matches=0) -> int:
+        if n==1:
+            return matches
+        elif n%2==0:
+            matches = self.numberOfMatches(n/2, matches+n/2)
+        else:
+            matches = self.numberOfMatches((n-1)/2+1, matches+(n-1)/2)
+        return int(matches)
